d6f4217268fccc65488646af41580a62bad01458
bstrubeXpanxion <bstrube@xpanxion.com>
2022-01-12 09:05:29 -0500

    added user class

---
+++ src/main/java/com/example/springbootapidemo/Users/User.java
@@ -0,0 +1,30 @@
+package com.example.springbootapidemo.Users;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    };
+
+    
+}
